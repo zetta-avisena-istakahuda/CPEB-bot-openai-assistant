425f07728998051844e486a39322c744d6a8f7d7
Avisena <avisena.huda@yahoo.co.id>
2023-11-13 14:47:07 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,87 @@
+import streamlit as st
+import os
+import openai
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+messages = ""
+if 'code_executed' not in st.session_state:
+    # Initialize the OpenAI client and create a thread
+    st.session_state.client = openai.OpenAI(api_key='')
+    st.session_state.thread = st.session_state.client.beta.threads.create()
+    st.session_state.code_executed = True
+
+# Streamlit app
+def main():
+    global messages
+    thread = st.session_state.thread
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    # Create a layout with two columns
+    left_column, right_column = st.columns([1, 3])
+
+    # Add an image to the left column
+    left_column.image("rubo-bot.png", use_column_width=True)
+
+    # Input field for the question
+    with right_column:
+        st.title(f"CPEB 2024 - Posez-nous vos questions...")
+        question = st.text_input("Entrez votre question:")
+        if st.button("Obtenir la réponse") or question:
+            # Check if a question is provided
+            if not question:
+                st.warning("Please enter a question.")
+            else:
+                # Generate and display the answer
+                while True:
+                    try:
+                        messages = question_answer(question)
+                        break  # Break out of the loop if the function call is successful
+                    except Exception as e:
+                        # Handle the exception (you might want to log it or print an error message)
+                        print(f"Error: {e}")
+                for msg in reversed(messages.data):
+                 role = msg.role
+                 content = msg.content[0].text.value
+                 if role.lower() == "user":
+                     background_color = "lightblue"
+                 elif role.lower() == "assistant":
+                     background_color = "lightgrey"
+                 else:
+                     background_color = "white"
+                 styled_content = f"<div style='background-color:{background_color}; padding:10px;'>{content}</div>"
+                 st.markdown(styled_content, unsafe_allow_html=True)
+
+
+def question_answer(question):
+ client = st.session_state.client
+ thread = st.session_state.thread
+ messages = ""
+ import time
+
+ message = client.beta.threads.messages.create(
+    thread_id = thread.id,
+    role = "user",
+    content = question
+)
+ run = client.beta.threads.runs.create(
+    thread_id = thread.id,
+    assistant_id = 'asst_ClB4u6msV6MOYyH57halU5cU',
+)
+ time.sleep(20)
+
+ run_status = client.beta.threads.runs.retrieve(
+  thread_id = thread.id,
+  run_id = run.id
+ )
+
+ messages = client.beta.threads.messages.list(
+  thread_id = thread.id
+  )
+
+
+ return(messages)
+
+if __name__ == "__main__":
+
+    main()
