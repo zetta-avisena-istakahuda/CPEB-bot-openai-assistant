7ed5506260da054fbbf4ea77886fb69eac67cc2c
zetta-avisena-istakahuda <121282835+zetta-avisena-istakahuda@users.noreply.github.com>
2023-11-16 15:05:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -84,9 +84,6 @@ def question_answer(question, isErrorParam):
     assistant_id = 'asst_ClB4u6msV6MOYyH57halU5cU',
     instructions = "Please provide the answer in bullet point format.  Always answer in French",
     tools=[{"type": "retrieval"}],
-    file_ids= [
-    "file-7QQupGwsQUM4koH98DdIxMPK",
-  ],
   )
   time.sleep(30)
   st.session_state.run_id = run.id 
@@ -112,9 +109,6 @@ def question_answer(question, isErrorParam):
     assistant_id = 'asst_ClB4u6msV6MOYyH57halU5cU',
     instructions = "Please provide the answer in bullet point format.  Always answer in French",
     tools=[{"type": "retrieval"}],
-    file_ids= [
-    "file-7QQupGwsQUM4koH98DdIxMPK",
-     ],
     )
        
    time.sleep(10) 
